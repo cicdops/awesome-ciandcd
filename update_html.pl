93a6d35e90e91f1fb755f5d8bdbf6968ad146d81
itech001 <itech001@126.com>
2014-11-29 20:47:50 -0500

    fix

--- update_html.pl
+++ update_html.pl
@@ -7,8 +7,8 @@ use Data::Dumper; # Perl core module
 # get md file
 my $giturl = "https://github.com/itech001/ciandcd.git";
 my $ret = 0;
-#$ret = system("rm -rf ciandcd");
-#$ret = system("git clone $giturl");
+$ret = system("rm -rf ciandcd");
+$ret = system("git clone $giturl");
 if($ret){
   print "Error:failed for git clone\n";
   exit 1;
